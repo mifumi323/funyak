b7336940bd1c0519214aeb6c1c44cff5402f8ea7
mifumi323 <mifumi323@gmail.com>
2016-03-04 07:45:15 +0900

    主人公のマップオブジェクト

---
+++ Core/MapObject/MainMapObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MifuminSoft.funyak.Core.MapObject
+{
+    class MainMapObject : IMapObject
+    {
+        public double Bottom
+        {
+            get;
+            protected set;
+        }
+
+        public double Left
+        {
+            get;
+            protected set;
+        }
+
+        public double Right
+        {
+            get;
+            protected set;
+        }
+
+        public double Top
+        {
+            get;
+            protected set;
+        }
+
+        public double X
+        {
+            get;
+            protected set;
+        }
+
+        public double Y
+        {
+            get;
+            protected set;
+        }
+    }
+}
