f9c809a7da45736ff61ce1db7f6f3bedfe9d261e
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-04 19:59:08 +0900

    Coreの単体テストプロジェクト

---
+++ CoreTests/MainMapObjectTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MifuminSoft.funyak.Core.MapObject;
+
+namespace MifuminSoft.funyak.Core.Tests
+{
+    [TestClass]
+    public class MainMapObjectTest
+    {
+        [TestMethod]
+        public void InitializeTest()
+        {
+            var mainMapObject = new MainMapObject(0, 0);
+        }
+    }
+}
