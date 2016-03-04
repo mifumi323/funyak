eb7a06e53057998e677768c74f3b7dfa73e2da88
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-04 13:04:02 +0900

    現状で不必要な参照を削除

--- Core/MapObject/MainMapObject.cs
+++ Core/MapObject/MainMapObject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MifuminSoft.funyak.Core.MapObject
+﻿namespace MifuminSoft.funyak.Core.MapObject
 {
     class MainMapObject : IMapObject
     {
