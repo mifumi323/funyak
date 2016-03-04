ddbb3ea50443c87863fbd71f020e801e3d182e79
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-04 20:01:41 +0900

    MainMapObjectの初期位置のテスト

--- CoreTests/MainMapObjectTest.cs
+++ CoreTests/MainMapObjectTest.cs
@@ -10,6 +10,12 @@ namespace MifuminSoft.funyak.Core.Tests
         public void InitializeTest()
         {
             var mainMapObject = new MainMapObject(0, 0);
+            Assert.AreEqual(0, mainMapObject.X);
+            Assert.AreEqual(0, mainMapObject.Y);
+            Assert.AreEqual(-14, mainMapObject.Left);
+            Assert.AreEqual(-14, mainMapObject.Top);
+            Assert.AreEqual(14, mainMapObject.Right);
+            Assert.AreEqual(14, mainMapObject.Bottom);
         }
     }
 }
