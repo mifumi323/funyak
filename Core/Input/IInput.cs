82d04726e15ee8c6ac58befefa4da543cb680577
mifumi323 <mifumi323@gmail.com>
2016-03-06 15:43:24 +0900

    入力インターフェース

---
+++ Core/Input/IInput.cs
@@ -0,0 +1,14 @@
+﻿namespace MifuminSoft.funyak.Core.Input
+{
+    public interface IInput
+    {
+        double X { get; }
+        double Y { get; }
+
+        bool IsPressed(int key);
+        bool IsPushed(int key);
+        bool IsReleased(int key);
+
+        bool Update();
+    }
+}
