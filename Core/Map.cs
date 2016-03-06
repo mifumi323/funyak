8c91e8163dbbf3c4d6b3192d6782f48495f8a5e3
mifumi323 <mifumi323@gmail.com>
2016-03-06 16:54:00 +0900

    IDynamicMapObjectをマップから動かそう

--- Core/Map.cs
+++ Core/Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MifuminSoft.funyak.Core.MapObject;
 
 namespace MifuminSoft.funyak.Core
@@ -9,6 +10,7 @@ namespace MifuminSoft.funyak.Core
         public double Height { get; protected set; }
 
         private ICollection<IMapObject> mapObjectCollection;
+        private ICollection<IDynamicMapObject> dynamicMapObjectCollection;
 
         public Map(double width, double height)
         {
@@ -16,15 +18,26 @@ namespace MifuminSoft.funyak.Core
             Height = height;
 
             mapObjectCollection = new List<IMapObject>();
+            dynamicMapObjectCollection = new List<IDynamicMapObject>();
         }
 
         public void AddMapObject(IMapObject mapObject)
         {
             mapObjectCollection.Add(mapObject);
+            if (mapObject is IDynamicMapObject) dynamicMapObjectCollection.Add((IDynamicMapObject)mapObject);
         }
 
         public void Update()
         {
+            UpdateMapObjects();
+        }
+
+        private void UpdateMapObjects()
+        {
+            foreach (var mapObject in dynamicMapObjectCollection)
+            {
+                mapObject.UpdateSelf();
+            }
         }
     }
 }
