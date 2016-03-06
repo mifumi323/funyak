a49b3fbcd877c15728b1e2dfee8681b7b76f4f06
mifumi323 <mifumi323@gmail.com>
2016-03-06 19:57:15 +0900

    マップのテスト

---
+++ CoreTests/MapTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MifuminSoft.funyak.Core.Tests
+{
+    [TestClass]
+    public class MapTest
+    {
+        [TestMethod]
+        public void InitializeTest()
+        {
+            var map = new Map(320, 224);
+            Assert.AreEqual(320, map.Width);
+            Assert.AreEqual(224, map.Height);
+        }
+    }
+}
