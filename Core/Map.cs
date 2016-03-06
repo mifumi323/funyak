12c60af119cf76734f0a97aeeed4c605e348d58d
mifumi323 <mifumi323@gmail.com>
2016-03-06 13:27:26 +0900

    マップオブジェクトのコレクション

--- Core/Map.cs
+++ Core/Map.cs
@@ -1,14 +1,26 @@
-﻿namespace MifuminSoft.funyak.Core
+﻿using System.Collections.Generic;
+using MifuminSoft.funyak.Core.MapObject;
+
+namespace MifuminSoft.funyak.Core
 {
     public class Map
     {
         public double Width { get; protected set; }
         public double Height { get; protected set; }
 
+        private ICollection<IMapObject> mapObjectCollection;
+
         public Map(double width, double height)
         {
             Width = width;
             Height = height;
+
+            mapObjectCollection = new List<IMapObject>();
+        }
+
+        public void AddMapObject(IMapObject mapObject)
+        {
+            mapObjectCollection.Add(mapObject);
         }
     }
 }
