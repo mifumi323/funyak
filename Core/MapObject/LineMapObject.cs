8e28cc977d1805ff411462dd4caadf23be1a61e9
mifumi323 <mifumi323@gmail.com>
2016-03-06 20:26:17 +0900

    LineMapObject

---
+++ Core/MapObject/LineMapObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MifuminSoft.funyak.Core.MapObject
+{
+    public class LineMapObject : IStaticMapObject
+    {
+        public LineMapObject(double x1, double y1, double x2, double y2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+
+        public double X { get { return (X1 + X2) / 2; } }
+        public double Y { get { return (Y1 + Y2) / 2; } }
+        public double Left { get { return Math.Min(X1, X2); } }
+        public double Right { get { return Math.Max(X1, X2); } }
+        public double Top { get { return Math.Min(Y1, Y2); } }
+        public double Bottom { get { return Math.Max(Y1, Y2); } }
+    }
+}
