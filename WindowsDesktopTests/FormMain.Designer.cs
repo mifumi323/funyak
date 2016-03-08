37cd91c90191892e8753fde88b970476a207d027
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-09 08:39:33 +0900

    MapViewのテスト(仮)

--- WindowsDesktopTests/FormMain.Designer.cs
+++ WindowsDesktopTests/FormMain.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "FormMain";
             this.Text = "WindowsDesktopテスト";
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsDesktopTests/FormMain.cs
+++ WindowsDesktopTests/FormMain.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MifuminSoft.funyak.Core;
+using MifuminSoft.funyak.Core.MapObject;
+using MifuminSoft.funyak.View;
 
 namespace WindowsDesktopTests
 {
@@ -15,6 +12,24 @@ namespace WindowsDesktopTests
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            var map = new Map(200, 200);
+            var main = new MainMapObject(100, 50);
+            var line = new LineMapObject(50, 150, 150, 150);
+            map.AddMapObject(main);
+            map.AddMapObject(line);
+            var img = new Bitmap((int)map.Width, (int)map.Height);
+            using (var mapView = new MapView(map))
+            {
+                using (var graphics = Graphics.FromImage(img))
+                {
+                    mapView.Display(graphics);
+                }
+            }
+            pictureBox1.Image = img;
         }
     }
 }
