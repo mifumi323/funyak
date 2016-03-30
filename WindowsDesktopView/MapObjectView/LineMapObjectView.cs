09a840e8bd8556a9cc610945b7a54f94c449403c
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-31 05:51:03 +0900

    LineMapObjectView

---
+++ WindowsDesktopView/MapObjectView/LineMapObjectView.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace MifuminSoft.funyak.View.MapObjectView
+{
+    public class LineMapObjectView : IMapObjectView
+    {
+        public int Priority { get { return -1; } }
+
+        public void Display(Graphics graphics)
+        {
+            // TODO: 線の表示処理
+        }
+    }
+}
