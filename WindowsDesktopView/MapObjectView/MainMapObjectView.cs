e189e0b500c52608372cb345f9fff4e82143aafe
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-31 05:49:39 +0900

    MainMapObjectView

---
+++ WindowsDesktopView/MapObjectView/MainMapObjectView.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace MifuminSoft.funyak.View.MapObjectView
+{
+    public class MainMapObjectView : IMapObjectView
+    {
+        public int Priority { get { return 0; } }
+
+        public void Display(Graphics graphics)
+        {
+            // TODO: 主人公の表示処理
+        }
+    }
+}
