535b0d6629571f154749efae053fe987639b782b
mifumi323 <mifumi323@tgws.fromc.jp>
2016-03-31 04:49:11 +0900

    IMapObjectView追加

---
+++ WindowsDesktopView/MapObjectView/IMapObjectView.cs
@@ -0,0 +1,11 @@
+﻿namespace MifuminSoft.funyak.View.MapObjectView
+{
+    public interface IMapObjectView
+    {
+        /// <summary>
+        /// 表示優先順位
+        /// 小さいほど先に描かれ、大きいほど手前に描かれる
+        /// </summary>
+        int Priority { get; }
+    }
+}
