f69025925fac9faba0c57dae9f92e466f5ec640d
mifumi323 <mifumi323@tgws.fromc.jp>
2016-05-18 00:21:49 +0900

    画面写真

--- WPFTests/MainWindow.xaml.cs
+++ WPFTests/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ImageMagick;
+using Microsoft.Win32;
 using MifuminSoft.funyak.View.Utility;
 
 namespace WPFTests
@@ -29,6 +31,36 @@ namespace WPFTests
         FpsCounter counter = new FpsCounter();
 
         double vx = 0, vy = 0, vr = 0, vs = 1;
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog()
+            {
+                Filter = "PNGファイル|*.png",
+                DefaultExt = "png",
+            };
+            if (dlg.ShowDialog(this) != true) return;
+
+            var canvas = this.canvas;
+            var size = new Size(canvas.ActualWidth, canvas.ActualHeight);
+            canvas.Measure(size);
+            canvas.Arrange(new Rect(size));
+
+            var renderBitmap = new RenderTargetBitmap((int)size.Width,
+                                                      (int)size.Height,
+                                                      96.0d,
+                                                      96.0d,
+                                                      PixelFormats.Pbgra32);
+            renderBitmap.Render(canvas);
+
+            string path = dlg.FileName;
+            using (var os = new FileStream(path, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+                encoder.Save(os);
+            }
+        }
 
         public MainWindow()
         {
