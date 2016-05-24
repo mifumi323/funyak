151038c31007a8fddee461c12715019558794947
mifumi323 <mifumi323@gmail.com>
2016-05-25 08:56:05 +0900

    入力なし

---
+++ Core/Input/NullInput.cs
@@ -0,0 +1,41 @@
+﻿namespace MifuminSoft.funyak.Core.Input
+{
+    /// <summary>入力なし</summary>
+    class NullInput : IInput
+    {
+        public double X
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public bool IsPressed(Keys key)
+        {
+            return false;
+        }
+
+        public bool IsPushed(Keys key)
+        {
+            return false;
+        }
+
+        public bool IsReleased(Keys key)
+        {
+            return false;
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
