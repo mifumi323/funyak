cf4e581ab6b276ae2ed7e62a612906d9f5ae5605
mifumi323 <mifumi323@gmail.com>
2016-06-07 23:58:20 +0900

    ImageChiInfoをちゃんと使える感じにした

--- WPFView/Resource/ImageChipInfo.cs
+++ WPFView/Resource/ImageChipInfo.cs
@@ -8,35 +8,35 @@
         /// <summary>
         /// 元画像から切り抜く範囲の左端のX座標(画像全体の左上を原点とする)
         /// </summary>
-        double SourceLeft;
+        public double SourceLeft { get; set; }
         /// <summary>
         /// 元画像から切り抜く範囲の上端のY座標(画像全体の左上を原点とする)
         /// </summary>
-        double SourceTop;
+        public double SourceTop { get; set; }
         /// <summary>
         /// 元画像から切り抜く範囲の幅
         /// </summary>
-        double SourceWidth;
+        public double SourceWidth { get; set; }
         /// <summary>
         /// 元画像から切り抜く範囲の高さ
         /// </summary>
-        double SourceHeight;
+        public double SourceHeight { get; set; }
         /// <summary>
         /// 表示の中心となる点の元画像におけるX座標(画像全体の左上を原点とする)
         /// </summary>
-        double SourceOriginX;
+        public double SourceOriginX { get; set; }
         /// <summary>
         /// 表示の中心となる点の元画像におけるY座標(画像全体の左上を原点とする)
         /// </summary>
-        double SourceOriginY;
+        public double SourceOriginY { get; set; }
 
         /// <summary>
         /// 表示先における画像チップの幅
         /// </summary>
-        double DestinationWidth;
+        public double DestinationWidth { get; set; }
         /// <summary>
         /// 表示先における画像チップの高さ
         /// </summary>
-        double DestinationHeight;
+        public double DestinationHeight { get; set; }
     }
 }
