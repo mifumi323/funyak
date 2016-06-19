7f5abdb07cba7713459f007fcd8c4fe7b5d596fc
mifumi323 <mifumi323@gmail.com>
2016-06-19 20:07:39 +0900

    SpriteSelector

---
+++ WpfUI/SpriteEditor/SpriteSelector.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MifuminSoft.funyak.UI.SpriteEditor
+{
+    /// <summary>
+    /// SpriteSelector.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SpriteSelector : UserControl
+    {
+        public SpriteSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
