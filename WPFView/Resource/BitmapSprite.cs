5e84c17cea7856fba17b0f1176ccd060b11a98ff
mifumi323 <mifumi323@gmail.com>
2016-06-19 21:10:25 +0900

    SpriteSelector>元画像表示

--- WPFView/Resource/BitmapSprite.cs
+++ WPFView/Resource/BitmapSprite.cs
@@ -27,8 +27,15 @@ namespace MifuminSoft.funyak.View.Resource
         public override Brush GetBrush(string key)
         {
             if (knownBrush.ContainsKey(key)) return knownBrush[key];
-            if (!Chip.ContainsKey(key)) throw new ArgumentOutOfRangeException(nameof(key));
-            var info = Chip[key];
+            SpriteChipInfo info;
+            try
+            {
+                info = GetChipInfo(key);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentOutOfRangeException(nameof(key));
+            }
             var brush = new ImageBrush(bitmapSource)
             {
                 TileMode = TileMode.None,
@@ -47,8 +54,15 @@ namespace MifuminSoft.funyak.View.Resource
 
         public override void SetToRectangle(Rectangle rectangle, string key, double x, double y, double scale, Transform transform)
         {
-            if (!Chip.ContainsKey(key)) throw new ArgumentOutOfRangeException(nameof(key));
-            var info = Chip[key];
+            SpriteChipInfo info;
+            try
+            {
+                info = GetChipInfo(key);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentOutOfRangeException(nameof(key));
+            }
             rectangle.Fill = GetBrush(key);
             rectangle.Width = info.DestinationWidth * scale;
             rectangle.Height = info.DestinationHeight * scale;
@@ -58,6 +72,28 @@ namespace MifuminSoft.funyak.View.Resource
             rectangle.RenderTransform = transform;
             Canvas.SetLeft(rectangle, x - info.DestinationWidth * scale * (info.SourceOriginX - info.SourceLeft) / info.SourceWidth);
             Canvas.SetTop(rectangle, y - info.DestinationHeight * scale * (info.SourceOriginY - info.SourceTop) / info.SourceHeight);
+        }
+
+        private SpriteChipInfo GetChipInfo(string key)
+        {
+            if (key == "")
+            {
+                return new SpriteChipInfo()
+                {
+                    SourceLeft = 0,
+                    SourceTop = 0,
+                    SourceWidth = bitmapSource.PixelWidth,
+                    SourceHeight = bitmapSource.PixelHeight,
+                    SourceOriginX = 0,
+                    SourceOriginY = 0,
+                    DestinationWidth = bitmapSource.PixelWidth,
+                    DestinationHeight = bitmapSource.PixelHeight,
+                };
+            }
+            else
+            {
+                return Chip[key];
+            }
         }
     }
 }
--- WpfUI/SpriteEditor/SpriteSelector.xaml.cs
+++ WpfUI/SpriteEditor/SpriteSelector.xaml.cs
@@ -12,12 +12,19 @@ namespace MifuminSoft.funyak.UI.SpriteEditor
         public Sprite Source
         {
             get { return (Sprite)GetValue(MyPropertyProperty); }
-            set { SetValue(MyPropertyProperty, value); }
+            set
+            {
+                SetValue(MyPropertyProperty, value);
+                if (value != null)
+                {
+                    value.SetToRectangle(background, "", 0, 0, 1.0);
+                }
+            }
         }
 
         // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty MyPropertyProperty =
-            DependencyProperty.Register(nameof(Source), typeof(Sprite), typeof(SpriteSelector), new PropertyMetadata(0));
+            DependencyProperty.Register(nameof(Source), typeof(Sprite), typeof(SpriteSelector), new PropertyMetadata(null));
 
         public SpriteSelector()
         {
