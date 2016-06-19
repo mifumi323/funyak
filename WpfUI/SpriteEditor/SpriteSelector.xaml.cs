2d3bcae64da15d49da7b32ebd0f42ce573712d8a
mifumi323 <mifumi323@gmail.com>
2016-06-19 20:30:47 +0900

    Sourceプロパティ

--- WpfUI/SpriteEditor/SpriteSelector.xaml.cs
+++ WpfUI/SpriteEditor/SpriteSelector.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using MifuminSoft.funyak.View.Resource;
 
 namespace MifuminSoft.funyak.UI.SpriteEditor
 {
     /// <summary>
-    /// SpriteSelector.xaml の相互作用ロジック
+    /// Spriteを編集のため表示・選択します
     /// </summary>
     public partial class SpriteSelector : UserControl
     {
+        public Sprite Source
+        {
+            get { return (Sprite)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register(nameof(Source), typeof(Sprite), typeof(SpriteSelector), new PropertyMetadata(0));
+
         public SpriteSelector()
         {
             InitializeComponent();
