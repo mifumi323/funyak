a0675e2654983a661e72ee01fa171690913db0a0
mifumi323 <mifumi323@tgws.fromc.jp>
2016-06-21 02:11:02 +0900

    ChipSelectorの説明とか

--- WpfUI/SpriteEditor/ChipSelector.xaml.cs
+++ WpfUI/SpriteEditor/ChipSelector.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -13,66 +14,80 @@ namespace MifuminSoft.funyak.UI.SpriteEditor
     /// </summary>
     public partial class ChipSelector : UserControl
     {
+        /// <summary>
+        /// 表示・選択するSpriteを取得または設定します
+        /// </summary>
+        [Description("表示・選択するSpriteを取得または設定します")]
         public Sprite Source
         {
             get { return (Sprite)GetValue(SourceProperty); }
             set { SetValue(SourceProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SourceProperty =
             DependencyProperty.Register(nameof(Source), typeof(Sprite), typeof(ChipSelector), new PropertyMetadata(null, new PropertyChangedCallback(OnSourceChanged)));
 
-
+        /// <summary>
+        /// 選択中の項目を取得または設定します
+        /// </summary>
+        [Description("選択中の項目を取得または設定します")]
         public KeyValuePair<string, SpriteChipInfo>? SelectedItem
         {
             get { return (KeyValuePair<string, SpriteChipInfo>?)GetValue(SelectedItemProperty); }
             set { SetValue(SelectedItemProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SelectedItemProperty =
             DependencyProperty.Register(nameof(SelectedItem), typeof(KeyValuePair<string, SpriteChipInfo>?), typeof(ChipSelector), new PropertyMetadata(null, new PropertyChangedCallback(OnSourceChanged)));
 
-
+        /// <summary>
+        /// 選択中のチップを取得します
+        /// </summary>
+        [Description("選択中のチップを取得します")]
         public SpriteChipInfo SelectedChip
         {
             get { return SelectedItem?.Value; }
         }
 
+        /// <summary>
+        /// 選択中のキーを取得します
+        /// </summary>
+        [Description("選択中のキーを取得します")]
         public string SelectedKey
         {
             get { return SelectedItem?.Key; }
         }
 
-
+        /// <summary>
+        /// 項目の表示色を取得または設定します
+        /// </summary>
+        [Description("項目の表示色を取得または設定します")]
         public Color ChipColor
         {
             get { return (Color)GetValue(ChipColorProperty); }
             set { SetValue(ChipColorProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ChipColorProperty =
             DependencyProperty.Register(nameof(ChipColor), typeof(Color), typeof(ChipSelector), new PropertyMetadata(Colors.Black, new PropertyChangedCallback(OnSourceChanged)));
 
-
+        /// <summary>
+        /// 選択中の項目の表示色を取得または設定します
+        /// </summary>
+        [Description("選択中の項目の表示色を取得または設定します")]
         public Color SelectedChipColor
         {
             get { return (Color)GetValue(SelectedChipColorProperty); }
             set { SetValue(SelectedChipColorProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SelectedChipColorProperty =
             DependencyProperty.Register(nameof(SelectedChipColor), typeof(Color), typeof(ChipSelector), new PropertyMetadata(Colors.Red, new PropertyChangedCallback(OnSourceChanged)));
-
 
         public ChipSelector()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 表示を更新します
+        /// </summary>
         public void Refresh()
         {
             canvas.Children.RemoveRange(1, canvas.Children.Count - 1);
