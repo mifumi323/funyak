1105798c5ad998ffcff0f3b5f300883cd3cf120d
mifumi323 <mifumi323@gmail.com>
2016-06-26 18:22:09 +0900

    UpdateMapObjectArgsにWidthとHeight追加

--- CoreCode/Map.cs
+++ CoreCode/Map.cs
@@ -7,52 +7,6 @@ using MifuminSoft.funyak.Core.MapObject;
 namespace MifuminSoft.funyak.Core
 {
     /// <summary>
-    /// マップオブジェクトの更新時の引数を格納します。
-    /// </summary>
-    public class UpdateMapObjectArgs
-    {
-        private Map map;
-
-        public UpdateMapObjectArgs(Map map)
-        {
-            this.map = map;
-        }
-
-        /// <summary>
-        /// 重力を取得します。
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        /// <returns>重力(0.0が無重力、1.0が通常)</returns>
-        public double GetGravity(double x, double y)
-        {
-            return map.GetGravity(x, y);
-        }
-
-        /// <summary>
-        /// 風速を取得します。
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        /// <returns>風速(0.0：無風、正の数：右向きの風、負の数：左向きの風)</returns>
-        public double GetWind(double x, double y)
-        {
-            return map.GetWind(x, y);
-        }
-
-        /// <summary>
-        /// 指定した位置の環境情報を取得します。
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        /// <returns>環境</returns>
-        public IMapEnvironment GetEnvironment(double x, double y)
-        {
-            return map.GetEnvironment(x, y);
-        }
-    }
-
-    /// <summary>
     /// ゲームのマップ
     /// </summary>
     public class Map : IMapEnvironment
---
+++ CoreCode/UpdateMapObjectArgs.cs
@@ -0,0 +1,60 @@
+﻿using MifuminSoft.funyak.Core.MapEnvironment;
+
+namespace MifuminSoft.funyak.Core
+{
+    /// <summary>
+    /// マップオブジェクトの更新時の引数を格納します。
+    /// </summary>
+    public class UpdateMapObjectArgs
+    {
+        private Map map;
+
+        public UpdateMapObjectArgs(Map map)
+        {
+            this.map = map;
+        }
+
+        /// <summary>
+        /// マップの幅を取得します。
+        /// </summary>
+        public double MapWidth { get { return map.Width; } }
+
+        /// <summary>
+        /// マップの高さを取得します。
+        /// </summary>
+        public double MapHeight { get { return map.Height; } }
+
+        /// <summary>
+        /// 重力を取得します。
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <returns>重力(0.0が無重力、1.0が通常)</returns>
+        public double GetGravity(double x, double y)
+        {
+            return map.GetGravity(x, y);
+        }
+
+        /// <summary>
+        /// 風速を取得します。
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <returns>風速(0.0：無風、正の数：右向きの風、負の数：左向きの風)</returns>
+        public double GetWind(double x, double y)
+        {
+            return map.GetWind(x, y);
+        }
+
+        /// <summary>
+        /// 指定した位置の環境情報を取得します。
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <returns>環境</returns>
+        public IMapEnvironment GetEnvironment(double x, double y)
+        {
+            return map.GetEnvironment(x, y);
+        }
+    }
+}
