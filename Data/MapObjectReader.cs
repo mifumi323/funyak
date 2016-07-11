08af0756b99829bca2fed1a7a12abc8101312582
mifumi323 <mifumi323@gmail.com>
2016-07-11 09:13:20 +0900

    線も読み込めたらいいなー

--- Data/MapObjectReader.cs
+++ Data/MapObjectReader.cs
@@ -12,17 +12,28 @@ namespace MifuminSoft.funyak.Data
 
         public static IMapObject FromDynamic(dynamic data)
         {
-            if (data.type == "funya")
-            {
-                return GenerateMainMapObject(data);
-            }
-            return null;
+            return
+                data.type == "funya" ? GenerateMainMapObject(data) :
+                data.type == "line" ? GenerateLineMapObject(data) :
+                null;
         }
 
         private static IMapObject GenerateMainMapObject(dynamic data)
         {
             var mainMapObject = new MainMapObject((double)(data.x ?? 0.0), (double)(data.y ?? 0.0));
             return mainMapObject;
+        }
+
+        private static IMapObject GenerateLineMapObject(dynamic data)
+        {
+            var lineMapObject = new LineMapObject((double)(data.x1 ?? 0.0), (double)(data.y1 ?? 0.0), (double)(data.x2 ?? 0.0), (double)(data.y2 ?? 0.0));
+            if (data.color != null) lineMapObject.Color = (string)data.color;
+            if (data.hit != null) lineMapObject.HitUpper = lineMapObject.HitBelow = lineMapObject.HitLeft = lineMapObject.HitRight = (bool)data.hit;
+            if (data.ht != null) lineMapObject.HitUpper = (bool)data.ht;
+            if (data.hb != null) lineMapObject.HitBelow = (bool)data.hb;
+            if (data.hl != null) lineMapObject.HitLeft = (bool)data.hl;
+            if (data.hr != null) lineMapObject.HitRight = (bool)data.hr;
+            return lineMapObject;
         }
     }
 }
