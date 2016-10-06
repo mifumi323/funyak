6ba2c90636b276ab51dbf5d16b88510c792adac7
mifumi323 <mifumi323@gmail.com>
2016-10-07 08:07:18 +0900

    WPF単体テスト

--- WPFTests/NormalUnitTest.xaml.cs
+++ WPFTests/NormalUnitTest.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,6 +15,30 @@ namespace WPFTests
     /// </summary>
     public partial class NormalUnitTest : Page
     {
+        public class TestCase
+        {
+            public Type TestClass { get; set; }
+            public MethodInfo TestMethod { get; set; }
+
+            public enum Result
+            {
+                None,
+                Success,
+                Failure,
+                Error,
+            }
+            public Result TestResult { get; set; } = Result.None;
+
+            public Exception TestException { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("{0}.{1}({2})", TestClass.Name, TestMethod.Name, TestResult);
+            }
+        }
+
+        public IList<TestCase> TestCaseList { get; set; } = new List<TestCase>();
+
         public NormalUnitTest()
         {
             InitializeComponent();
@@ -26,9 +53,65 @@ namespace WPFTests
                 var testMethods = testClass.GetMethods().Where(m => m.GetCustomAttribute<TestMethodAttribute>() != null);
                 foreach (var testMethod in testMethods)
                 {
-                    lbTestCase.Items.Add(string.Format("{0}.{1}", testClass.Name, testMethod.Name));
+                    TestCaseList.Add(new TestCase()
+                    {
+                        TestClass = testClass,
+                        TestMethod = testMethod,
+                    });
                 }
             }
+            lbTestCase.ItemsSource = TestCaseList;
+        }
+
+        private void btnTestSelected_Click(object sender, RoutedEventArgs e)
+        {
+            Test(lbTestCase.SelectedItems.Cast<TestCase>());
+        }
+
+        private void btnTestAll_Click(object sender, RoutedEventArgs e)
+        {
+            Test(TestCaseList);
+        }
+
+        private void Test(IEnumerable<TestCase> testCaseList)
+        {
+            Task.Run(async () =>
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    btnTestSelected.IsEnabled = false;
+                    btnTestAll.IsEnabled = false;
+                });
+                foreach (var testCase in testCaseList)
+                {
+                    try
+                    {
+                        var testObject = Activator.CreateInstance(testCase.TestClass);
+                        testCase.TestMethod.Invoke(testObject, new object[0]);
+                        testCase.TestResult = TestCase.Result.Success;
+                        testCase.TestException = null;
+                    }
+                    catch (AssertFailedException e)
+                    {
+                        testCase.TestResult = TestCase.Result.Failure;
+                        testCase.TestException = e;
+                    }
+                    catch (Exception e)
+                    {
+                        testCase.TestResult = TestCase.Result.Error;
+                        testCase.TestException = e;
+                    }
+                    await Dispatcher.InvokeAsync(() =>
+                    {
+                        lbTestCase.Items.Refresh();
+                    });
+                }
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    btnTestSelected.IsEnabled = true;
+                    btnTestAll.IsEnabled = true;
+                });
+            });
         }
     }
 }
