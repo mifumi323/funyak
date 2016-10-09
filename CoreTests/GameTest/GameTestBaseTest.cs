a115923797b74b5f7c0f44dda4beb6e2cb72a323
mifumi323 <mifumi323@gmail.com>
2016-10-09 21:29:35 +0900

    GameTestBase自体のテスト

---
+++ CoreTests/GameTest/GameTestBaseTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MifuminSoft.funyak.Core.Tests.GameTest
+{
+    /// <summary>
+    /// GameTestBase自体のテスト
+    /// </summary>
+    [TestClass]
+    public class GameTestBaseTest : GameTestBase
+    {
+        public override bool IsSuccess()
+        {
+            return false;
+        }
+
+        [TestMethod]
+        public void FailWhenMapFileIsNull()
+        {
+            MapFilePath = null;
+            FailOnTimeout = true;
+            TimeoutFrames = 1;
+            try
+            {
+                Run();
+            }
+            catch (ArgumentNullException)
+            {
+                return;
+            }
+            Assert.Fail("テスト用ファイルがnullでも失敗になりませんでした。");
+        }
+
+        [TestMethod]
+        public void FailWhenFailToReadMapFile()
+        {
+            MapFilePath = @"NoSuchFile";
+            FailOnTimeout = true;
+            TimeoutFrames = 1;
+            try
+            {
+                Run();
+            }
+            catch (FileNotFoundException)
+            {
+                return;
+            }
+            Assert.Fail("テスト用ファイルが存在しなくても失敗になりませんでした。");
+        }
+
+        [TestMethod]
+        public void FailOnTimeoutTest()
+        {
+            MapFilePath = @"TestFiles\GameTestBaseTest.json";
+            FailOnTimeout = true;
+            TimeoutFrames = 100;
+            try
+            {
+                Run();
+            }
+            catch (AssertFailedException ex)
+            {
+                if (ex.Message.Contains("テストが規定時間以内に成功しませんでした。"))
+                {
+                    return;
+                }
+                throw;
+            }
+            Assert.Fail("テストが規定時間以内に成功しなくても失敗になりませんでした。");
+        }
+
+        [TestMethod]
+        public void SucceedOnTimeoutTest()
+        {
+            MapFilePath = @"TestFiles\GameTestBaseTest.json";
+            FailOnTimeout = false;
+            TimeoutFrames = 100;
+            try
+            {
+                Run();
+            }
+            catch (AssertFailedException ex)
+            {
+                if (ex.Message.Contains("テストが規定時間以内に成功しませんでした。"))
+                {
+                    Assert.Fail("テストが規定時間以内に失敗しなくても成功になりませんでした。");
+                }
+                throw;
+            }
+        }
+    }
+}
