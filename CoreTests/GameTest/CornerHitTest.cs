7d9765bf0e8eb4f2a619d8e1b4d2999ae64414a0
mifumi323 <mifumi323@gmail.com>
2016-12-06 09:27:28 +0900

    すり抜けのテスト(作りかけ)
    すり抜けといてSuccessなわけねーだろ！

---
+++ CoreTests/GameTest/CornerHitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MifuminSoft.funyak.Core.Tests.GameTest
+{
+    [TestClass]
+    public class CornerHitTest : GameTestBase
+    {
+        public CornerHitTest()
+        {
+            MapFilePath = @"TestFiles\CornerHitTest.json";
+            FailOnTimeout = false;
+            TimeoutFrames = 100;
+        }
+
+        public override bool IsSuccess()
+        {
+            return false;
+        }
+
+        [TestMethod]
+        public override void Run()
+        {
+            base.Run();
+        }
+    }
+}
