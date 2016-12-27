65a25d71dd1b3d238db1d20de0de49bac5f91814
mifumi323 <mifumi323@tgws.fromc.jp>
2016-12-28 04:58:40 +0900

    交点計算復活

--- CoreCode/CollisionHelper/Collision2D.cs
+++ CoreCode/CollisionHelper/Collision2D.cs
@@ -35,5 +35,43 @@
 
             return -DELTA <= t1 && t1 <= 1 + DELTA && -DELTA <= t2 && t2 <= 1 + DELTA;
         }
+
+        /// <summary>
+        /// 線分と線分の交差判定を行います。
+        /// 参考：http://marupeke296.com/COL_2D_No10_SegmentAndSegment.html
+        /// </summary>
+        /// <param name="s1">線分1</param>
+        /// <param name="s2">線分2</param>
+        /// <param name="crossPoint">交差していた場合に交点の位置ベクトルを格納するVector2Dへの参照</param>
+        /// <returns>交差していればtrue</returns>
+        public static bool SegmentSegment(Segment2D s1, Segment2D s2, ref Vector2D crossPoint)
+        {
+            var v1 = s1.End - s1.Start;
+            var v2 = s2.End - s2.Start;
+            var crs_v1_v2 = v1.Cross(v2);
+
+            // 平行
+            if (crs_v1_v2 == 0)
+            {
+                return false;
+            }
+
+            var v = s2.Start - s1.Start;
+            var crs_v_v1 = v.Cross(v1);
+            var crs_v_v2 = v.Cross(v2);
+
+            var t1 = crs_v_v2 / crs_v1_v2;
+            var t2 = crs_v_v1 / crs_v1_v2;
+
+            if (-DELTA <= t1 && t1 <= 1 + DELTA && -DELTA <= t2 && t2 <= 1 + DELTA)
+            {
+                crossPoint = s1.Start + v1 * t1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- CoreTests/CollisionHelperTest.cs
+++ CoreTests/CollisionHelperTest.cs
@@ -28,5 +28,36 @@ namespace MifuminSoft.funyak.Core.Tests
                 Assert.IsFalse(Collision2D.SegmentSegment(s1, s2), "平行なパターンで失敗");
             }
         }
+
+        [TestMethod]
+        public void SegmentSegmentCrossPointTest()
+        {
+            // 交差しているパターン
+            {
+                var s1 = new Segment2D(10, 10, 20, 20);
+                var s2 = new Segment2D(10, 20, 20, 10);
+                var cp = new Vector2D(double.NaN, double.NaN);
+                var result = Collision2D.SegmentSegment(s1, s2, ref cp);
+                Assert.IsTrue(result, "交差しているパターンで失敗");
+                Assert.AreEqual(15, cp.X, 0.0001, "交差しているパターンで失敗");
+                Assert.AreEqual(15, cp.Y, 0.0001, "交差しているパターンで失敗");
+            }
+            // 交差していないパターン
+            {
+                var s1 = new Segment2D(10, 10, 20, 20);
+                var s2 = new Segment2D(110, 120, 120, 110);
+                var cp = new Vector2D(double.NaN, double.NaN);
+                var result = Collision2D.SegmentSegment(s1, s2, ref cp);
+                Assert.IsFalse(result, "交差していないパターンで失敗");
+            }
+            // 平行なパターン
+            {
+                var s1 = new Segment2D(10, 10, 20, 20);
+                var s2 = new Segment2D(110, 10, 120, 20);
+                var cp = new Vector2D(double.NaN, double.NaN);
+                var result = Collision2D.SegmentSegment(s1, s2, ref cp);
+                Assert.IsFalse(result, "平行なパターンで失敗");
+            }
+        }
     }
 }
