3d12bbcf9721d1f30f5b66cc62666bc3ff113132
mifumi323 <mifumi323@gmail.com>
2018-03-13 23:35:50 +0900

    Demo 1

---
+++ WpfDemo1/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using MifuminSoft.funyak;
+using MifuminSoft.funyak.Data;
+using MifuminSoft.funyak.Input;
+using MifuminSoft.funyak.MapEnvironment;
+using MifuminSoft.funyak.MapObject;
+using MifuminSoft.funyak.View;
+using MifuminSoft.funyak.View.Input;
+using MifuminSoft.funyak.View.MapObject;
+using MifuminSoft.funyak.View.Resource;
+using MifuminSoft.funyak.View.Timing;
+
+namespace WpfDemo1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ElapsedFrameCounter frameCounter = new ElapsedFrameCounter();
+        Map map = null;
+        MapView mapView = null;
+        IInput input;
+        Sprite resource;
+        IMapObject mainObject = null;
+        AreaEnvironment goalArea = null;
+        bool goaled = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            resource = SpriteReader.Read(@"Assets\main.png");
+            input = new KeyInput();
+
+            ReadMap(File.ReadAllText(@"Assets\demo1map.json"));
+        }
+
+        private void ReadMap(string data)
+        {
+            map = MapReader.FromString(data, new MapReaderOption()
+            {
+                Input = input,
+            });
+            mainObject = map.FindMapObject("main") ?? map.GetMapObjects().FirstOrDefault();
+            goalArea = map.FindAreaEnvironment("goal");
+            canvas.Children.Clear();
+            mapView = new MapView(map, new MapObjectViewFactory()
+            {
+                MainMapObjectResourceSelector = a => resource,
+            })
+            {
+                Canvas = canvas,
+                FocusTo = mainObject
+            };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            CompositionTarget.Rendering -= CompositionTarget_Rendering;
+        }
+
+        private void CompositionTarget_Rendering(object sender, object e)
+        {
+            var frames = frameCounter.GetElapsedFrames(true);
+            if (frames > 0 && map != null)
+            {
+                for (int i = 0; i < frames; i++)
+                {
+                    input.Update();
+                    map.Update();
+
+                    if (!goaled)
+                    {
+                        if (map.GetEnvironment(mainObject.X, mainObject.Y) == goalArea)
+                        {
+                            goaled = true;
+                            label.Visibility = Visibility.Visible;
+                        }
+                    }
+                }
+                mapView.Update(1.0);
+            }
+        }
+    }
+}
---
+++ WpfDemo1/readme.txt
@@ -0,0 +1,33 @@
+﻿# funyak Demo 1
+
+funyakの現状を見てもらうために作ったプログラムです。
+ゲームとしてはまだ成り立っていませんが、とりあえずゴールとか作ってゲームっぽくしてみました。
+左上がゴールなので目指してみてください。
+
+## 操作説明
+←→　ふにゃが走るよ！
+↓　　ふにゃがしゃがむよ！しゃがみながら歩くと風を受けないよ！
+↑　　落ちるのがちょっと遅くなるよ！
+Ｚ　　ジャンプするよ！押す時間が「短い」と高く飛ぶよ！
+
+## このデモのギミックとか
+坂道　　　　登れるよ！
+赤いエリア　風が吹いてるよ！色が濃いとやばいよ！
+青い床　　　下からすり抜けるよ！
+白いエリア　無重力だよ！Ｚでわりときびきび動くよ！
+黒いエリア　高重力だよ！ジャンプが低くなるよ！
+左上エリア　ゴールだよ！「GOAL!!」って表示されるよ！
+
+
+## 著作権
+(C) 2018 Mifumi Matsuda
+https://tgws.plus/
+https://github.com/mifumi323/funyak
+
+## ライセンス
+Apache License 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+
+## 権利表記等
+- Magick.NET (C) 2013-2016 Dirk Lemstra <https://magick.codeplex.com/>
+- Newtonsoft.Json (C) 2007 James Newton-King
