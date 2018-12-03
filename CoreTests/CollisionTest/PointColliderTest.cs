d4f8b1f95d4d1311cd6aa75a9295ed6186e755b7
mifumi323 <mifumi323@gmail.com>
2018-12-04 08:11:55 +0900

    [分割]CollisionTest→テスト対象クラスごとのテストクラスに

---
+++ CoreTests/CollisionTest/PointColliderTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MifuminSoft.funyak.Collision;
+
+namespace MifuminSoft.funyak.Core.Tests.CollisionTest
+{
+    [TestClass]
+    public class PointColliderTest
+    {
+        [TestMethod]
+        public void SetPointTest()
+        {
+            var collider = new PointCollider(null);
+            collider.SetPoint(100.0, 200.0);
+            Assert.AreEqual(100.0, collider.X, 0.0001, "PointCollider.Xが不正");
+            Assert.AreEqual(200.0, collider.Y, 0.0001, "PointCollider.Yが不正");
+            Assert.AreEqual(100.0, collider.Left, 0.0001, "PointCollider.Leftが不正");
+            Assert.AreEqual(200.0, collider.Top, 0.0001, "PointCollider.Topが不正");
+            Assert.AreEqual(100.0, collider.Right, 0.0001, "PointCollider.Rightが不正");
+            Assert.AreEqual(200.0, collider.Bottom, 0.0001, "PointCollider.Bottomが不正");
+        }
+    }
+}
---
+++ CoreTests/CollisionTest/RectangleColliderTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MifuminSoft.funyak.Collision;
+
+namespace MifuminSoft.funyak.Core.Tests.CollisionTest
+{
+    [TestClass]
+    public class RectangleColliderTest
+    {
+        [TestMethod]
+        public void SetPositionTest()
+        {
+            var collider = new RectangleCollider(null);
+            collider.SetPosition(100.0, 200.0, 300.0, 400.0);
+            Assert.AreEqual(100.0, collider.Left, 0.0001, "RectangleCollider.Leftが不正");
+            Assert.AreEqual(200.0, collider.Top, 0.0001, "RectangleCollider.Topが不正");
+            Assert.AreEqual(300.0, collider.Right, 0.0001, "RectangleCollider.Rightが不正");
+            Assert.AreEqual(400.0, collider.Bottom, 0.0001, "RectangleCollider.Bottomが不正");
+        }
+    }
+}
