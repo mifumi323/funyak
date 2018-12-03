8257b77ff5c21330e34ffbe97a13b15d73752825
mifumi323 <mifumi323@gmail.com>
2018-12-04 08:21:06 +0900

    [追加]RectangleColliderTest.ContainsTest

--- CoreTests/CollisionTest/RectangleColliderTest.cs
+++ CoreTests/CollisionTest/RectangleColliderTest.cs
@@ -16,5 +16,36 @@ namespace MifuminSoft.funyak.Core.Tests.CollisionTest
             Assert.AreEqual(300.0, collider.Right, 0.0001, "RectangleCollider.Rightが不正");
             Assert.AreEqual(400.0, collider.Bottom, 0.0001, "RectangleCollider.Bottomが不正");
         }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            var collider = new RectangleCollider(null);
+            collider.SetPosition(200.0, 300.0, 400.0, 500.0);
+            var testCases = new[]
+            {
+                // 真ん中
+                new { X = 300.0, Y=400.0, Expects = true },
+                // 四隅
+                new { X = 200.0, Y=300.0, Expects = true },
+                new { X = 400.0, Y=300.0, Expects = true },
+                new { X = 400.0, Y=500.0, Expects = true },
+                new { X = 200.0, Y=500.0, Expects = true },
+                // 辺上
+                new { X = 200.0, Y=400.0, Expects = true },
+                new { X = 300.0, Y=300.0, Expects = true },
+                new { X = 400.0, Y=400.0, Expects = true },
+                new { X = 300.0, Y=500.0, Expects = true },
+                // 外
+                new { X = 100.0, Y=400.0, Expects = false },
+                new { X = 300.0, Y=200.0, Expects = false },
+                new { X = 500.0, Y=400.0, Expects = false },
+                new { X = 300.0, Y=600.0, Expects = false },
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(testCase.Expects, collider.Contains(testCase.X, testCase.Y), $"({testCase.X}, {testCase.Y})で失敗");
+            }
+        }
     }
 }
