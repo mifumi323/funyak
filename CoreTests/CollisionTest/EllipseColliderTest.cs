d0040ab4d7fec56633543b665936e8cc33736978
mifumi323 <mifumi323@gmail.com>
2018-12-04 22:34:34 +0900

    [追加]EllipseColliderTest.ContainsWideTest
    これまで真円や面積0という特殊な条件しかテストしていなかったため。
    XとYを取り違えるみたいな、真円だと気付きにくいケースに対応。

--- CoreTests/CollisionTest/EllipseColliderTest.cs
+++ CoreTests/CollisionTest/EllipseColliderTest.cs
@@ -59,6 +59,48 @@ namespace MifuminSoft.funyak.Core.Tests.CollisionTest
         }
 
         [TestMethod]
+        public void ContainsWideTest()
+        {
+            // 真円じゃない場合
+            var collider = new EllipseCollider(null);
+            collider.SetPosition(400.0, 300.0, 800.0, 500.0);
+            var testCases = new[]
+            {
+                // 真ん中
+                new { X = 600.0, Y=400.0, Expects = true },
+                // AABB四隅
+                new { X = 400.0, Y=300.0, Expects = false },
+                new { X = 800.0, Y=300.0, Expects = false },
+                new { X = 800.0, Y=500.0, Expects = false },
+                new { X = 400.0, Y=500.0, Expects = false },
+                // AABB内接点
+                new { X = 400.0, Y=400.0, Expects = true },
+                new { X = 600.0, Y=300.0, Expects = true },
+                new { X = 800.0, Y=400.0, Expects = true },
+                new { X = 600.0, Y=500.0, Expects = true },
+                // 真ん中以外の内部
+                new { X = 460.0, Y=330.0, Expects = true },
+                new { X = 740.0, Y=330.0, Expects = true },
+                new { X = 740.0, Y=470.0, Expects = true },
+                new { X = 460.0, Y=470.0, Expects = true },
+                // AABB内部の楕円外部
+                new { X = 440.0, Y=320.0, Expects = false },
+                new { X = 760.0, Y=320.0, Expects = false },
+                new { X = 760.0, Y=480.0, Expects = false },
+                new { X = 440.0, Y=480.0, Expects = false },
+                // 完全に外
+                new { X = 300.0, Y=400.0, Expects = false },
+                new { X = 600.0, Y=200.0, Expects = false },
+                new { X = 900.0, Y=400.0, Expects = false },
+                new { X = 600.0, Y=600.0, Expects = false },
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(testCase.Expects, collider.Contains(testCase.X, testCase.Y), $"({testCase.X}, {testCase.Y})で失敗");
+            }
+        }
+
+        [TestMethod]
         public void ContainsWidth0Test()
         {
             var collider = new EllipseCollider(null);
