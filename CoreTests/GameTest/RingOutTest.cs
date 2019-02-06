f9e28ebf5ce966ecc426b76cb5d0602bab4d638a
mifumi323 <mifumi323@gmail.com>
2019-02-07 07:52:53 +0900

    [追加]RingOutTest

---
+++ CoreTests/GameTest/RingOutTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MifuminSoft.funyak.MapObject;
+
+namespace MifuminSoft.funyak.Core.Tests.GameTest
+{
+    [TestClass]
+    public class RingOutTest : GameTestBase
+    {
+        public RingOutTest()
+        {
+            MapFilePath = @"TestFiles\RingOutTest.json";
+            FailOnTimeout = true;
+            TimeoutFrames = 100;
+        }
+
+        public override bool IsSuccess()
+        {
+            return new string[] { "A", "B", "C", "D" }.All(id => (Map.FindMapObject(id) as MainMapObject).State == MainMapObjectState.Die);
+        }
+
+        [TestMethod]
+        public override void Run() => base.Run();
+    }
+}
