4d55ebc909c528e09d8cadf6897de8e5af9a935f
mifumi323 <mifumi323@gmail.com>
2019-02-08 23:56:41 +0900

    [追加]RegionMapObject
    AreaEnvironmentの代替を目指す。
    #15 AreaEnvironmentとMapObjectの統合

---
+++ CoreCode/MapObject/RegionMapObject.cs
@@ -0,0 +1,23 @@
+﻿using MifuminSoft.funyak.Collision;
+
+namespace MifuminSoft.funyak.MapObject
+{
+    public sealed class RegionMapObject : MapObjectBase
+    {
+        public RegionCollider Collider;
+        public string BackgroundColor;
+
+        public override double X
+        {
+            get => (Collider.Left + Collider.Right) * 0.5;
+            set => Collider.Shift(value - X, 0.0);
+        }
+        public override double Y
+        {
+            get => (Collider.Top + Collider.Bottom) * 0.5;
+            set => Collider.Shift(0.0, value - Y);
+        }
+        public override void OnJoin(Map map, CollisionManager collisionManager) => collisionManager.Add(Collider);
+        public override void OnLeave(Map map, CollisionManager collisionManager) => collisionManager.Remove(Collider);
+    }
+}
