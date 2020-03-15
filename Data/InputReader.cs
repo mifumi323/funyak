1e4723153f75f7b425a68be4245459f65e6a5757
mifumi323 <mifumi323@gmail.com>
2020-03-15 23:54:17 +0900

    Readerへの不正な入力には例外を返すように

--- Data/InputReader.cs
+++ Data/InputReader.cs
@@ -1,4 +1,5 @@
-﻿using MifuminSoft.funyak.Input;
+﻿using System;
+using MifuminSoft.funyak.Input;
 
 namespace MifuminSoft.funyak.Data
 {
@@ -7,7 +8,7 @@ namespace MifuminSoft.funyak.Data
         public static IInput FromDynamic(dynamic data, MapReaderOption option) =>
             // TODO: ReplayInputも作ろう
             data.type == "arrange" ? GenerateArrangeInput(data, option) :
-                null;
+                throw new ArgumentException($"不明なdata.type\"{data.type}\"です。");
 
         private static IInput GenerateArrangeInput(dynamic data, MapReaderOption option) => new ArrangeInput(option.Input)
         {
