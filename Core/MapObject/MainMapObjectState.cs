97e4542987911b2551f08593fd8c4c6130285539
mifumi323 <mifumi323@gmail.com>
2021-03-12 21:44:27 +0900

    MainMapObject関係の型を別々のファイルに分離
    #45

---
+++ Core/MapObject/MainMapObjectState.cs
@@ -0,0 +1,22 @@
+﻿namespace MifuminSoft.funyak.MapObject
+{
+    /// <summary>
+    /// 主人公のマップオブジェクトの状態
+    /// </summary>
+    public enum MainMapObjectState
+    {
+        Stand,
+        Run,
+        Walk,
+        Charge,
+        Jump,
+        Fall,
+        Float,
+        BreatheIn,
+        BreatheOut,
+        Tired,
+        Frozen,
+        Damaged,
+        Die,
+    }
+}
