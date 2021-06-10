b2647af73236a9f30f47664a54483bdf36cfa148
mifumi323 <mifumi323@gmail.com>
2021-06-10 20:30:03 +0900

    [追加]YamlHelper
    YamlDotNetを直接使うと割とコード量が多くなってしまうため。
    #36

---
+++ Data/YamlHelper.cs
@@ -0,0 +1,22 @@
+﻿using YamlDotNet.RepresentationModel;
+
+namespace MifuminSoft.funyak.Data
+{
+    public static class YamlHelper
+    {
+        public static double GetDouble(this YamlMappingNode mappingNode, YamlNode key, double defaultValue)
+        {
+            return mappingNode.Children.ContainsKey(key) &&
+                mappingNode.Children[key] is YamlScalarNode valueNode &&
+                double.TryParse(valueNode.Value, out var value) ?
+                value : defaultValue;
+        }
+
+        public static string GetString(this YamlMappingNode mappingNode, YamlNode key, string defaultValue)
+        {
+            return mappingNode.Children.ContainsKey(key) &&
+                mappingNode.Children[key] is YamlScalarNode valueNode  ?
+                valueNode.Value ?? defaultValue : defaultValue;
+        }
+    }
+}
