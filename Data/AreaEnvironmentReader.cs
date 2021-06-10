9462581e76afaeb0048ac78133d121a1a2befbe6
mifumi323 <mifumi323@gmail.com>
2021-06-10 23:10:35 +0900

    [追加]AreaEnvironmentをYamlで読み込む
    AreaEnvironment自体がもう廃止予定だけど、一応ね。
    #36

--- Data/AreaEnvironmentReader.cs
+++ Data/AreaEnvironmentReader.cs
@@ -21,9 +21,19 @@ namespace MifuminSoft.funyak.Data
             BackgroundColor = data.c,
         };
 
-        public static AreaEnvironment? FromYamlNode(YamlNode areaNode, MapReaderOption option)
+        public static AreaEnvironment? FromYamlNode(YamlNode areaNode, MapReaderOption option) => areaNode is YamlMappingNode mappingNode ? new AreaEnvironment()
         {
-            throw new NotImplementedException();
-        }
+            Name = mappingNode.GetStringOrNull("n"),
+
+            Left = mappingNode.GetDouble("l", double.NegativeInfinity),
+            Top = mappingNode.GetDouble("t", double.NegativeInfinity),
+            Right = mappingNode.GetDouble("r", double.PositiveInfinity),
+            Bottom = mappingNode.GetDouble("b", double.PositiveInfinity),
+
+            Gravity = mappingNode.GetDouble("g", double.NaN),
+            Wind = mappingNode.GetDouble("w", double.NaN),
+
+            BackgroundColor = mappingNode.GetStringOrNull("c"),
+        } : throw new ArgumentException("areaのデータ構造はマッピングノードである必要があります。", nameof(areaNode));
     }
 }
