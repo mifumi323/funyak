e7115df6345a25252b7a5af3f9507d22e04f1fb4
mifumi323 <mifumi323@gmail.com>
2021-06-10 23:15:25 +0900

    [追加]InputをYamlで読み込む
    #36

--- Data/InputReader.cs
+++ Data/InputReader.cs
@@ -21,7 +21,23 @@ namespace MifuminSoft.funyak.Data
 
         public static IInput FromYamlNode(YamlNode inputNode, MapReaderOption option)
         {
-            throw new NotImplementedException();
+            if (!(inputNode is YamlMappingNode mappingNode))
+            {
+                throw new ArgumentException("inputのデータ構造はマッピングノードである必要があります。", nameof(inputNode));
+            }
+            var type = mappingNode.GetStringOrNull("type");
+            // TODO: ReplayInputも作ろう
+            return type switch
+            {
+                "arrange" => GenerateArrangeInput(mappingNode, option),
+                _ => throw new ArgumentException($"不明なarea.type\"{type}\"です。")
+            };
         }
+
+        private static IInput GenerateArrangeInput(YamlMappingNode objectNode, MapReaderOption option) => new ArrangeInput(option.Input)
+        {
+            HorizontalReverse = objectNode.GetBoolean("hr", false),
+            VerticalReverse = objectNode.GetBoolean("vr", false),
+        };
     }
 }
