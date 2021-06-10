3b3931b0bc3d1d7a2e45cca464c7c8bc0bfa0c5b
mifumi323 <mifumi323@gmail.com>
2021-06-10 23:18:10 +0900

    [削除]JSONとdynamicによるMap読み込み周りの処理
    JSON自体はほかでも使っている場所があるので、Newtonsoft.Jsonは消さない。
    #36

--- Data/InputReader.cs
+++ Data/InputReader.cs
@@ -6,19 +6,6 @@ namespace MifuminSoft.funyak.Data
 {
     class InputReader
     {
-        public static IInput FromDynamic(dynamic data, MapReaderOption option) => (string)data.type switch
-        // TODO: ReplayInputも作ろう
-        {
-            "arrange" => GenerateArrangeInput(data, option),
-            _ => throw new ArgumentException($"不明なdata.type\"{data.type}\"です。")
-        };
-
-        private static IInput GenerateArrangeInput(dynamic data, MapReaderOption option) => new ArrangeInput(option.Input)
-        {
-            HorizontalReverse = data.hr ?? false,
-            VerticalReverse = data.vr ?? false,
-        };
-
         public static IInput FromYamlNode(YamlNode inputNode, MapReaderOption option)
         {
             if (!(inputNode is YamlMappingNode mappingNode))
