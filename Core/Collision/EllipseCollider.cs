db501e2b9c79e7b289db1c40666bad580b2df7b5
mifumi323 <mifumi323@gmail.com>
2021-07-17 20:00:28 +0900

    [移動]RegionInfoをRegionColliderに
    #14

--- Core/Collision/EllipseCollider.cs
+++ Core/Collision/EllipseCollider.cs
@@ -4,8 +4,6 @@ namespace MifuminSoft.funyak.Collision
 {
     public sealed class EllipseCollider : RegionCollider
     {
-        public RegionInfo RegionInfo { get; private set; } = new RegionInfo();
-
         public EllipseCollider(MapObjectBase owner) : base(owner) { }
 
         public void SetPosition(double left, double top, double right, double bottom) => UpdatePosition(left, top, right, bottom);
