34af1277c0a676703d56e6a1fba3f914e0b2771e
mifumi323 <mifumi323@gmail.com>
2021-07-20 21:24:46 +0900

    [変更]フラグの処理を拡張メソッドに

--- Core/Collision/PlateInfo.cs
+++ Core/Collision/PlateInfo.cs
@@ -5,7 +5,7 @@
         public PlateAttributeFlag Flags { get; set; }
         public double Friction { get; set; }
 
-        public bool HasFlag(PlateAttributeFlag flag) => (Flags & flag) != PlateAttributeFlag.None;
-        public void SetFlag(PlateAttributeFlag flag, bool value) => Flags = value ? (Flags | flag) : (Flags & ~flag);
+        public bool HasFlag(PlateAttributeFlag flag) => Flags.Has(flag);
+        public void SetFlag(PlateAttributeFlag flag, bool value) => Flags = Flags.SetTo(flag, value);
     }
 }
