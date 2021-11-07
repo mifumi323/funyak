63c33647727bb28b943e762e2d990bf5257a268b
mifumi323 <mifumi323@gmail.com>
2021-11-07 20:12:42 +0900

    [テスト]SandwichedFunyaTest
    プルプルしたら死ぬテスト。
    #38

---
+++ UnitTests/Game/SandwichedFunyaTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace MifuminSoft.funyak.UnitTests.Game
+{
+    public class SandwichedFunyaTest : GameTestBase
+    {
+        public SandwichedFunyaTest()
+        {
+            MapFilePath = "SandwichedFunyaTest.yml";
+            FailOnTimeout = false;
+            TimeoutFrames = 200;
+        }
+
+        public override bool IsSuccess()
+        {
+            var main = Map.FindMapObject("main");
+            Assert.IsNotNull(main, "mainが見つかりません。");
+            if (main.Y > 100)
+            {
+                // プルプルしたら死ぬ
+                Assert.AreEqual(100, main.X, 0.1);
+            }
+            return false;
+        }
+
+        [Test]
+        public override void Run()
+        {
+            base.Run();
+        }
+    }
+}
