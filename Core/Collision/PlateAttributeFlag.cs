5a93746fa5bbd87ae16352eb68ba32778d460bc1
mifumi323 <mifumi323@gmail.com>
2022-03-07 21:07:53 +0900

    [コメント追加]PlateAttributeFlag
    PlateとNeedleで立場が逆でややこしいので。

--- Core/Collision/PlateAttributeFlag.cs
+++ Core/Collision/PlateAttributeFlag.cs
@@ -6,9 +6,13 @@ namespace MifuminSoft.funyak.Collision
     public enum PlateAttributeFlag : ulong
     {
         None = 0,
+        /// <summary>上向きのPlate(Needle側は下向き)</summary>
         HitUpper = 1 << 0,
+        /// <summary>下向きのPlate(Needle側は上向き)</summary>
         HitBelow = 1 << 1,
+        /// <summary>左向きのPlate(Needle側は右向き)</summary>
         HitLeft = 1 << 2,
+        /// <summary>左向きのPlate(Needle側は右向き)</summary>
         HitRight = 1 << 3,
     }
 
