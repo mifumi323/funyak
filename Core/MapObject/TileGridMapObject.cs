ddafd165c4ee2ce7136b59df9ed6253cde9aa3fa
mifumi323 <mifumi323@gmail.com>
2022-12-19 21:39:50 +0900

    [変更]TileChipをPlateInfo対応に
    #52

--- Core/MapObject/TileGridMapObject.cs
+++ Core/MapObject/TileGridMapObject.cs
@@ -7,12 +7,39 @@ namespace MifuminSoft.funyak.MapObject
 {
     public class TileChip
     {
+        public readonly PlateInfo PlateInfo;
+
         public object? Resource;
-        public bool HitUpper;
-        public bool HitBelow;
-        public bool HitLeft;
-        public bool HitRight;
-        public double Friction;
+        public bool HitUpper
+        {
+            get => PlateInfo.HasFlag(PlateAttributeFlag.HitUpper);
+            set => PlateInfo.SetFlag(PlateAttributeFlag.HitUpper, value);
+        }
+        public bool HitBelow
+        {
+            get => PlateInfo.HasFlag(PlateAttributeFlag.HitBelow);
+            set => PlateInfo.SetFlag(PlateAttributeFlag.HitBelow, value);
+        }
+        public bool HitLeft
+        {
+            get => PlateInfo.HasFlag(PlateAttributeFlag.HitLeft);
+            set => PlateInfo.SetFlag(PlateAttributeFlag.HitLeft, value);
+        }
+        public bool HitRight
+        {
+            get => PlateInfo.HasFlag(PlateAttributeFlag.HitRight);
+            set => PlateInfo.SetFlag(PlateAttributeFlag.HitRight, value);
+        }
+        public double Friction
+        {
+            get => PlateInfo.Friction;
+            set => PlateInfo.Friction = value;
+        }
+
+        public TileChip(PlateInfo? plateInfo = null)
+        {
+            PlateInfo = plateInfo ?? new PlateInfo();
+        }
     }
 
     public class TileGridMapObject : MapObjectBase, IBounds
