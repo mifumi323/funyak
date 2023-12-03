84c6811d62ac15fe930c5542b8fcb17e1e0875a2
mifumi323 <mifumi323@gmail.com>
2023-12-03 23:52:28 +0900

    UpdateMapObjectArgsのIMapEnvironment関連の機能削除

--- Core/UpdateMapObjectArgs.cs
+++ Core/UpdateMapObjectArgs.cs
@@ -22,33 +22,9 @@ namespace MifuminSoft.funyak
         public double MapHeight => map.Height;
 
         /// <summary>
-        /// 重力を取得します。
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        /// <returns>重力(0.0が無重力、1.0が通常)</returns>
-        public double GetGravity(double x, double y) => map.GetGravity(x, y);
-
-        /// <summary>
         /// マップの重力を取得します。
         /// </summary>
         /// <returns>重力(0.0が無重力、1.0が通常)</returns>
         public double MapGravity => map.Gravity;
-
-        /// <summary>
-        /// 風速を取得します。
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        /// <returns>風速(0.0：無風、正の数：右向きの風、負の数：左向きの風)</returns>
-        public double GetWind(double x, double y) => map.GetWind(x, y);
-
-        /// <summary>
-        /// 指定した位置の環境情報を取得します。
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        /// <returns>環境</returns>
-        public IMapEnvironment GetEnvironment(double x, double y) => map.GetEnvironment(x, y);
     }
 }
